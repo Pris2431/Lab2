faf8bc7dc42143dcedf3742c69f96075771ca88f
Priscilla <PRISCILLALOI.23@ichat.sp.edu.sg>
2024-04-23 18:41:39 +0800

    added median temp and sort temp function

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,4 @@
+import statistics
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g 5, 67, 32)")
 
@@ -10,7 +11,6 @@ def get_user_input():
     return numlist
 
 def cal_average(numlist):
-    print("cal_average")
     total_sum = 0
     for i in range (len(numlist)):
         total_sum = total_sum + numlist[i]
@@ -20,27 +20,35 @@ def cal_average(numlist):
     return average
 
 def find_min_max(numlist):
-    min = numlist[0]
-    max = numlist[0]
+    min_temp = numlist[0]
+    max_temp = numlist[0]
     for i in range (len(numlist)):
-        if numlist[i] < min:
-            min = numlist[i]
-        if numlist[i] > max:
-            max = numlist[i]
-    print("min = " + str(min))
-    print("max = " + str(max))
-    return min, max
+        if numlist[i] < min_temp:
+            min_temp = numlist[i]
+        if numlist[i] > max_temp:
+            max_temp = numlist[i]
+    print("Min temperature= " + str(min_temp))
+    print("Max temperature = " + str(max_temp))
+    return min_temp, max_temp
 
-def sort_temperature():
-    print("sort_temperature")
+def sort_temperature (numlist):
+    numlist= sorted(numlist)
+    return sorted(numlist)
 
-def calc_median_temperature():
-    print("calc_median_temperature")
+def calc_median_temperature(numlist):
+    median_temp = statistics.median(sorted(numlist))
+    median_temp = str(median_temp)
+    print("Median Temperature = " + median_temp)
+    return median_temp
+
 
 def main():
+    display_main_menu()
     numlist= get_user_input()
     cal_average(numlist)
     find_min_max(numlist)
+    sort_temperature(numlist)
+    calc_median_temperature(numlist)
 
 if __name__ == "__main__":
     main()
